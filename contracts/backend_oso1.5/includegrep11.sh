0f02bd2976dbc115cb5d476d561af43f9af0e1db
jeanyves-girard <jeanyves.girard@fr.ibm.com>
2026-03-16 23:32:23 +0100

    remove grep11_addon

--- contracts/backend_oso1.5/includegrep11.sh
+++ contracts/backend_oso1.5/includegrep11.sh
@@ -8,10 +8,6 @@ done
 sed -e "s/HSMDOMAIN/$HSMDOMAIN1/" -e "s/EP11SERVERPORT/10876/" grep11server.tpl > srv/grep11server1.yaml
 sed -e "s/HSMDOMAIN/$HSMDOMAIN2/" -e "s/EP11SERVERPORT/11876/" grep11server.tpl > srv/grep11server2.yaml
 
-GREP11DIR=grep11_addon
-
-cp terraform_grep11.tfvars $GREP11DIR/terraform.tfvars
-
 for i in cfg nginx srv 
 do 
 	cp -r $i docker-compose/
