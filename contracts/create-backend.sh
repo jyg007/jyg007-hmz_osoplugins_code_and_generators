d0c7d04b89fae29b288dc296206325d4ef3995b1
jeanyves-girard <jeanyves.girard@fr.ibm.com>
2026-08-20 14:49:05 +0200

    pod branch

--- contracts/create-backend.sh
+++ contracts/create-backend.sh
@@ -19,6 +19,9 @@ pushd "$contract_root" || exit 1
 
 pushd backend || exit 1
 # shellcheck disable=SC2154
+./includegrep11.sh
+#mv env-crypto.yml ..
+
 ${tf} init && ${tf} destroy -auto-approve && ${tf} apply -auto-approve
 ${CP} -rf backend.yml ../output/backend/user-data
 popd || exit 1
